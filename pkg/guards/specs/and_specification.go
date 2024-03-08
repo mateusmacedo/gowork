c73231b117988d6e0bb48fcbe00b0040439c4d5f
Mateus Macedo Dos Anjos <mateus.anjos@dock.tech>
2024-03-08 01:30:45 -0300

    docs(specs): :books: add documentation for `IsSatisfiedBy` method in and specification

--- pkg/guards/specs/and_specification.go
+++ pkg/guards/specs/and_specification.go
@@ -9,6 +9,8 @@ func NewAndSpecification(specs ...Specification[Candidate]) *AndSpecification {
     return &AndSpecification{specs: specs}
 }
 
+// IsSatisfiedBy verifica se a especificação é satisfeita por um determinado candidato.
+// Retorna true se todas as especificações forem satisfeitas, caso contrário, retorna false.
 func (s *AndSpecification) IsSatisfiedBy(candidate Candidate) bool {
     for _, spec := range s.specs {
         if !spec.IsSatisfiedBy(candidate) {
