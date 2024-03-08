c1cee3755df0a2e2c592a27212fd843b90231281
Mateus Macedo Dos Anjos <mateus.anjos@dock.tech>
2024-03-08 01:44:13 -0300

    test(specs): 🧪 add tests for orSpecification's IsSatisfiedBy method

--- pkg/guards/specs/or_specification_test.go
+++ pkg/guards/specs/or_specification_test.go
@@ -49,3 +49,63 @@ func TestNewOrSpecification(t *testing.T) {
 		})
 	}
 }
+
+func TestOrSpecification_IsSatisfiedBy(t *testing.T) {
+	tests := []struct {
+		name   string
+		specs []specification.Specification[specification.Candidate]
+		candidate   specification.Candidate
+		want   bool
+	}{
+		{
+			name:      "satisfied by almost one specifications",
+			specs:     []specification.Specification[specification.Candidate]{
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
+						// Implemente sua lógica de avaliação aqui
+						// Por exemplo, sempre retornar true para fins de teste
+						return true
+					},
+				),
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
+						// Implemente sua lógica de avaliação aqui
+						// Por exemplo, sempre retornar true para fins de teste
+						return false
+					},
+				),
+			},
+			candidate: &struct{}{}, // Substitua pelo candidato real, se necessário
+			want:      true,
+		},
+		{
+			name:      "not satisfied by all specifications",
+			specs:     []specification.Specification[specification.Candidate]{
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
+						// Implemente sua lógica de avaliação aqui
+						// Por exemplo, sempre retornar true para fins de teste
+						return false
+					},
+				),
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
+						// Implemente sua lógica de avaliação aqui
+						// Por exemplo, sempre retornar true para fins de teste
+						return false
+					},
+				),
+			},
+			candidate: &struct{}{}, // Substitua pelo candidato real, se necessário
+			want:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sut := specification.NewOrSpecification(tt.specs...)
+			if got := sut.IsSatisfiedBy(tt.candidate); got != tt.want {
+				t.Errorf("OrSpecification.IsSatisfiedBy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
