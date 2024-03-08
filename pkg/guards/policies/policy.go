8b2f2963d0effe35f318e012e29aae071debd360
Mateus Macedo Dos Anjos <mateus.anjos@dock.tech>
2024-03-08 07:48:34 -0300

    feat(policies): :sparkles: add policy handling for rule application and combination

---
+++ pkg/guards/policies/policy.go
@@ -0,0 +1,62 @@
+package policies
+
+import (
+	"errors"
+
+	"gowork/pkg/guards/rules"
+)
+
+type Policy[T any, R any] struct {
+	rules []rules.Rule[T, R]
+}
+
+func NewPolicy[T any, R any](rules ...rules.Rule[T, R]) *Policy[T, R] {
+	return &Policy[T, R]{rules: rules}
+}
+
+func (p *Policy[T, R]) AddRule(r rules.Rule[T, R]) {
+	p.rules = append(p.rules, r)
+}
+
+func (p *Policy[T, R]) ApplyRules(target T) (R, error) {
+	var lastResult R
+	var err error
+	hasApplied := false
+
+	// Combine all rules into a single rule before applying
+	var combinedRule rules.Rule[T, R]
+	if len(p.rules) > 0 {
+		combinedRule = p.rules[0]
+		for _, r := range p.rules[1:] {
+			combinedRule = combinedRule.Combine(r)
+		}
+		lastResult, err = combinedRule.Apply(target)
+		hasApplied = true
+	} else {
+		return *new(R), errors.New("no rules to apply")
+	}
+
+	if err != nil {
+		return *new(R), err
+	}
+
+	if !hasApplied {
+		return *new(R), errors.New("no rules applied")
+	}
+
+	return lastResult, nil
+}
+
+func (p *Policy[T, R]) BatchApplyRules(targets []T) ([]R, []error) {
+	if len(p.rules) == 0 {
+		return nil, []error{errors.New("no rules to apply")}
+	}
+
+	// Combine all rules into a single rule before batch applying
+	combinedRule := p.rules[0]
+	for _, r := range p.rules[1:] {
+		combinedRule = combinedRule.Combine(r)
+	}
+
+	return combinedRule.BatchApply(targets)
+}
