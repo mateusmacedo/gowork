c053fa4ff2a9e6300c17a5142eba251698a937aa
Mateus Macedo Dos Anjos <mateus.anjos@dock.tech>
2024-03-08 01:26:16 -0300

    test(specification): 🧪 add unit tests for `AndSpecification.IsSatisfiedBy` method

--- pkg/guards/specs/and_specification_test.go
+++ pkg/guards/specs/and_specification_test.go
@@ -49,3 +49,49 @@ func TestNewAndSpecification(t *testing.T) {
 		})
 	}
 }
+
+func TestAndSpecification_IsSatisfiedBy(t *testing.T) {
+	tests := []struct {
+		name      string
+		specs     []specification.Specification[specification.Candidate]
+		candidate specification.Candidate
+		want      bool
+	}{
+		{
+			name:      "satisfied by all specifications",
+			specs:     []specification.Specification[specification.Candidate]{
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
+						// Implemente sua lógica de avaliação aqui
+						// Por exemplo, sempre retornar true para fins de teste
+						return true
+					},
+				),
+			},
+			candidate: &struct{}{}, // Substitua pelo candidato real, se necessário
+			want:      true,
+		},
+		{
+			name:      "not satisfied by all specifications",
+			specs:     []specification.Specification[specification.Candidate]{
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
+						// Implemente sua lógica de avaliação aqui
+						// Por exemplo, sempre retornar true para fins de teste
+						return false
+					},
+				),
+			},
+			candidate: &struct{}{}, // Substitua pelo candidato real, se necessário
+			want:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sut := specification.NewAndSpecification(tt.specs...)
+			if got := sut.IsSatisfiedBy(tt.candidate); got != tt.want {
+				t.Errorf("AndSpecification.IsSatisfiedBy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
