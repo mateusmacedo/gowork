77d5a05156f14510b347642ea5a4f6b678d3c3f8
Mateus Macedo Dos Anjos <mateus.anjos@dock.tech>
2024-03-08 02:07:14 -0300

    test(guards/specs): :rotating_light: streamline and_coherence tests and improve readability

--- pkg/guards/specs/and_specification_test.go
+++ pkg/guards/specs/and_specification_test.go
@@ -8,42 +8,31 @@ import (
 )
 
 func TestNewAndSpecification(t *testing.T) {
-	// Definir uma função de chamada para a especificação dummy
-	callable := func(candidate specification.Candidate) bool {
-		// Implemente sua lógica de avaliação aqui
-		// Por exemplo, sempre retornar true para fins de teste
-		return true
+	specs := []specification.Specification[specification.Candidate]{
+		fixtures.NewDummySpecification(func(candidate specification.Candidate) bool {
+			return true
+		}),
+		fixtures.NewDummySpecification(func(candidate specification.Candidate) bool {
+			return true
+		}),
 	}
 
-	// Criar uma especificação dummy usando pacote fixtures
-	dummySpec := fixtures.NewDummySpecification(callable)
-
-	// Criar uma lista de especificações para passar para NewAndSpecification
-	specs := []specification.Specification[specification.Candidate]{dummySpec}
-
-	// Criar a expectativa de uma instância de AndSpecification
 	want := specification.NewAndSpecification(specs...)
 
 	tests := []struct {
-		name string
-		args struct {
-			specs []specification.Specification[specification.Candidate]
-		}
-		want *specification.AndSpecification
+		name  string
+		specs []specification.Specification[specification.Candidate]
+		want  *specification.AndSpecification
 	}{
 		{
-			name: "Testando a criação da AndSpecification com uma especificação dummy",
-			args: struct {
-				specs []specification.Specification[specification.Candidate]
-			}{
-				specs: specs,
-			},
-			want: want,
+			name:  "Testando a criação da AndSpecification com uma especificação dummy",
+			specs: specs,
+			want:  want,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := specification.NewAndSpecification(tt.args.specs...); !reflect.DeepEqual(got, tt.want) {
+			if got := specification.NewAndSpecification(tt.specs...); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewAndSpecification() = %v, want %v", got, tt.want)
 			}
 		})
@@ -58,31 +47,37 @@ func TestAndSpecification_IsSatisfiedBy(t *testing.T) {
 		want      bool
 	}{
 		{
-			name:      "satisfied by all specifications",
-			specs:     []specification.Specification[specification.Candidate]{
+			name: "satisfied by all specifications",
+			specs: []specification.Specification[specification.Candidate]{
 				fixtures.NewDummySpecification(
 					func(candidate specification.Candidate) bool {
-						// Implemente sua lógica de avaliação aqui
-						// Por exemplo, sempre retornar true para fins de teste
+						return true
+					},
+				),
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
 						return true
 					},
 				),
 			},
-			candidate: &struct{}{}, // Substitua pelo candidato real, se necessário
+			candidate: &struct{}{},
 			want:      true,
 		},
 		{
-			name:      "not satisfied by all specifications",
-			specs:     []specification.Specification[specification.Candidate]{
+			name: "not satisfied by all specifications",
+			specs: []specification.Specification[specification.Candidate]{
 				fixtures.NewDummySpecification(
 					func(candidate specification.Candidate) bool {
-						// Implemente sua lógica de avaliação aqui
-						// Por exemplo, sempre retornar true para fins de teste
 						return false
 					},
 				),
+				fixtures.NewDummySpecification(
+					func(candidate specification.Candidate) bool {
+						return true
+					},
+				),
 			},
-			candidate: &struct{}{}, // Substitua pelo candidato real, se necessário
+			candidate: &struct{}{},
 			want:      false,
 		},
 	}
