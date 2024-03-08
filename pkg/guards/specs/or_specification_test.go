5d366dd9b5efee5b3eca30d19284eea83cb71211
Mateus Macedo Dos Anjos <mateus.anjos@dock.tech>
2024-03-08 01:41:19 -0300

    test(specs): 🧪 add tests for `OrSpecification` creation with dummy spec

---
+++ pkg/guards/specs/or_specification_test.go
@@ -0,0 +1,51 @@
+package specification_test
+
+import (
+	"gowork/pkg/guards/fixtures"
+	specification "gowork/pkg/guards/specs"
+	"reflect"
+	"testing"
+)
+
+func TestNewOrSpecification(t *testing.T) {
+	// Definir uma função de chamada para a especificação dummy
+	callable := func(candidate specification.Candidate) bool {
+		// Implemente sua lógica de avaliação aqui
+		// Por exemplo, sempre retornar true para fins de teste
+		return true
+	}
+
+	// Criar uma especificação dummy usando pacote fixtures
+	dummySpec := fixtures.NewDummySpecification(callable)
+
+	// Criar uma lista de especificações para passar para NewOrSpecification
+	specs := []specification.Specification[specification.Candidate]{dummySpec}
+
+	// Criar a expectativa de uma instância de OrSpecification
+	want := specification.NewOrSpecification(specs...)
+
+	tests := []struct {
+		name string
+		args struct {
+			specs []specification.Specification[specification.Candidate]
+		}
+		want *specification.OrSpecification
+	}{
+		{
+			name: "Testando a criação da OrSpecification com uma especificação dummy",
+			args: struct {
+				specs []specification.Specification[specification.Candidate]
+			}{
+				specs: specs,
+			},
+			want: want,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := specification.NewOrSpecification(tt.args.specs...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewOrSpecification() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
